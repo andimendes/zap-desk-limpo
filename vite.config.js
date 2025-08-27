c5e80c6fb016e86b2bd38f5cd7a05b4386a15f60
andimendes <anderson@gdelfos.com>
2025-08-26 23:38:23 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // Importar o módulo 'path' do Node.js
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Adicionar a configuração 'resolve' para criar o alias
+  resolve: {
+    alias: {
+      // O símbolo '@' agora vai apontar para o diretório 'src'
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
