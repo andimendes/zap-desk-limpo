5bbaecb3b8fb5e77315d804d30cf63fda3cebbba
andimendes <anderson@gdelfos.com>
2025-08-30 12:27:00 +0000

    ajuste tela branca

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' // 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // O StrictMode foi desativado para garantir a compatibilidade com a biblioteca
+  // de arrastar e soltar (@hello-pangea/dnd), que pode causar erros quando está ativo.
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  // </React.StrictMode>,
+);
+
