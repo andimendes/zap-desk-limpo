4b38ab6b1d218e66ceb9894e88e1ab7b910333e6
andimendes <anderson@gdelfos.com>
2025-09-16 00:40:28 +0000

    refactor: Migra para Tailwind CSS v3 para estabilidade

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-// postcss.config.js
-export default {
+module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {}, // <-- O novo nome correto
+    tailwindcss: {},
     autoprefixer: {},
   },
 }
